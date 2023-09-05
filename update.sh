be115e755228ab5ae2b82f66535ff958c13ea820
Print With Smile <123603173+PrintWithSmile@users.noreply.github.com>
2023-09-05 10:42:04 +0200

    small fix

--- update.sh
+++ update.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-
-ARCHIVE_FOLDER="/home/pi/printer_data/config/Archive"
-PWS_FOLDER="/home/pi/printer_data/config/PWS_config"
-ALL_CONFIG="/home/pi/printer_data/config"
-
-	
 echo "Zálohuji předchozí konfigurace"
-cd "$ALL_CONFIG"
-zip -r "zaloha_$(date +"%d-%m-%Y").zip" /home/pi/printer_data/config/* -x "/home/pi/printer_data/config/Archive/*"
-mv "zaloha_$(date +"%d-%m-%Y").zip" "$ARCHIVE_FOLDER"
+cd /home/pi/printer_data/config
+zip -r "zaloha_$(date +"%d-%m-%Y").zip" /home/pi/printer_data/config/* -x "/home/pi/printer_data/config/Archive/*" -x "/home/pi/printer_data/config/Archive"
+mv "zaloha_$(date +"%d-%m-%Y").zip" /home/pi/printer_data/config/Archive
 cp -f /home/pi/PWS/PWS_400K_CZ/Konfigurace/* /home/pi/printer_data/config/PWS_config/
 
 echo "Restartuji klipper pro načtení nových konfigurací"
