dac9696eb1e01c5a770a22e9a318bcc9fa37a9cc
Print With Smile <123603173+PrintWithSmile@users.noreply.github.com>
2023-09-14 22:19:42 +0200

    Kalibrační tisky

--- update.sh
+++ update.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get install -y zip
+
+if ! command -v zip &> /dev/null; then
+    sudo apt-get update
+    sudo apt-get install zip -y
+
+    if [ $? -eq 0 ]; then
+        echo "ZIP utility installed successfully."
+    else
+        echo "Failed to install ZIP utility."
+        exit 1
+    fi
+fi
 echo "Zálohuji předchozí konfigurace"
 cd /home/pi/printer_data/config
 zip -r "zaloha_$(date +"%d-%m-%Y").zip" /home/pi/printer_data/config/* -x "/home/pi/printer_data/config/Archive/*" -x "/home/pi/printer_data/config/Archive"
