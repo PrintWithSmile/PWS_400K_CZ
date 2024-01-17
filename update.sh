448e13d5fd930424d02add69249802c6c83d9c1e
Print With Smile <123603173+PrintWithSmile@users.noreply.github.com>
2024-01-17 21:51:07 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -141,8 +141,8 @@ if [ -e "$FILE_PATH" ]; then
         echo "Content replaced successfully."
     else
         echo "Error: The file $FILE_PATH exists but does not contain the expected content."
-		echo orangepi1234 | sudo -S echo -e "$NEW_SCRIPT" > "$FILE_PATH"
-        echo orangepi1234 | sudo -S chmod 755 "$FILE_PATH"
+		echo "orangepi1234" | sudo -S echo -e "$NEW_SCRIPT" > "$FILE_PATH"
+        echo "orangepi1234" | sudo -S chmod 755 "$FILE_PATH"
         echo "Content replaced successfully."
     fi
 else
